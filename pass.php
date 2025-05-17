8fd1c6417fda9cd939ca95f3e0120ac42489c57b
irleo <leomargarzon29@gmail.com>
2025-05-18 00:05:35 +0800

    .

--- pass.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo password_hash('admin_pass', PASSWORD_DEFAULT) . PHP_EOL;
-echo password_hash('customer_pass', PASSWORD_DEFAULT) . PHP_EOL;
-echo password_hash('provider_pass', PASSWORD_DEFAULT) . PHP_EOL;
-?>
